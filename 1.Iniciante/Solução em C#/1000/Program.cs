cf3a454a2afbdff7c9feb55cc317c1daf3d69478
Pablo Ribeiro <pabloribeiro.cont@gmail.com>
2024-11-12 11:24:37 -0300

    12-11

--- 1.Iniciante/Solução em C#/1000/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-class URI {
-    static void Main(string[] args) {
-        Console.WriteLine("Olá, Mundo!"); // Exibe uma mensagem na tela
-    }
-}
---
+++ 1.Iniciante/Solução em C/1000.c
@@ -0,0 +1,11 @@
+// beecrowd | 1000 - Hello World!
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello World!\n");
+
+    return 0;
+}
