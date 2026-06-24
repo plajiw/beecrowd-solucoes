7dbe8e3c96aa3d000067b829d4a4f65e68ec68f7
plajiw <pabloribeiro.cont@gmail.com>
2026-06-24 07:19:40 -0300

    add: 1009.py

---
+++ submissoes/python/1009.py
@@ -0,0 +1,7 @@
+# beecrowd | 1009 - Salary with Bonus
+
+# -*- coding: utf-8 -*-
+
+name, fixed_salary, total = input(), float(input()), float(input())
+salary = fixed_salary + total * 0.15
+print(f"TOTAL = R$ {salary:.2f}")
